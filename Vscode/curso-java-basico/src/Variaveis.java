fbe618def23322e7f1d4b085760cf47cb6adf2c7
Gustavo <83922725+GustavoGiglio@users.noreply.github.com>
2023-02-10 23:47:24 -0300

    Aula10
    Código realizado durante a aula sobre introdução a variáveis e suas boas práticas,

---
+++ Vscode/curso-java-basico/src/Variaveis.java
@@ -0,0 +1,12 @@
+public class Variaveis {
+    public static void main(String[] args) throws Exception {
+        
+        int idade = 20;
+        String nome = "Gustavo";
+        String nomeDoMeuCachorro = "Rex";
+
+        System.out.println("Idade = " + idade);
+        System.out.println("Nome = " + nome);
+        System.out.println("Nome Cachorro = " + nomeDoMeuCachorro);
+    }
+}
