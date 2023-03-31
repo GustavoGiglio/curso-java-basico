eb502f5ee33fd8767634006fd50dce07671f4486
Gustavo <83922725+GustavoGiglio@users.noreply.github.com>
2023-03-30 22:06:03 -0300

    add aula 28

---
+++ Vscode/curso-java-basico/src/aula28/Pacotes.java
@@ -0,0 +1,18 @@
+package aula28;
+
+import aula20.exercicios.Exercicio04;
+import aula20.exercicios.Exercicio05;
+import aula20.exercicios.Exercicio06;
+import aula27.Carro;
+
+public class Pacotes {
+    public static void main(String[] args) {
+        
+        Carro carro;
+        Carro carro2;
+
+        Exercicio06 exer06;
+        Exercicio05 exer05;
+        Exercicio04 exer04;
+    }
+}
