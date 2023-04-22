7c9c4914451fed4a0173221c263c0ecbba71b641
GustavoGiglio <gtsgiglio@gmail.com>
2023-04-22 01:01:57 -0300

    add aula 42 - final e constate

---
+++ Vscode/curso-java-basico/src/aula42/Constantes.java
@@ -0,0 +1,8 @@
+package aula42;
+
+public final class Constantes {
+    
+    public static final String URL_BLOG = "https://loiane.com";
+    public static final String CURSO_COMPLETO = "https://loiane.traingcom";
+    
+}
